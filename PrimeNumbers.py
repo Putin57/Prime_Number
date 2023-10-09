0fd3fcd168188488acf98c8a46cb64e90900093e
Mahidul Islam <75845610+Putin57@users.noreply.github.com>
2023-10-09 12:04:39 +0600

    Update PrimeNumbers.py

--- PrimeNumbers.py
+++ PrimeNumbers.py
@@ -1,10 +1,15 @@
-def isPrime(n):
+def is_prime(n):
     if n==0 or n==1:
         return False
-    for i in range(2,n):
+    divisor=0
+    for i in range(1,n+1):
         if n%i==0:
-            return False
-    return True
+            divisor+=1
+    if divisor==2:return True
+    else:return False
+    
+
 for i in range(1,int(input())+1):
-    if isPrime(i):
-        print(i)
+    if is_prime(i):
+        print(i,end=" ")
+print()
